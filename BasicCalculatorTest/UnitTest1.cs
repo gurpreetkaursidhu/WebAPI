fa4ef1362e999ce0474c1206bbb81c7d53390b90
gurpreetkaur <ginnisstudyroomm07@gmail.com>
2022-10-18 03:17:08 -0400

    Addition Test added

--- BasicCalculatorTest/UnitTest1.cs
+++ BasicCalculatorTest/UnitTest1.cs
@@ -1,10 +1,26 @@
-﻿namespace BasicCalculatorTest;
+﻿using CalculatorLogic;
+namespace BasicCalculatorTest;
+
 
 [TestClass]
 public class UnitTest1
 {
+    
     [TestMethod]
-    public void TestMethod1()
+    [DataRow(1, 2, 3)]
+    [DataRow(1, 1, 2)]
+    [DataRow(3, 8, 11)]
+    [DataRow(2, 2, 4)]
+    [DataRow(3, 3, 6)]
+    [DataRow(4, 1, 5)]
+    [DataRow(9, 6.7, 15.7)]
+    [DataRow(1.2, 1, 2.2)]
+    [DataRow(1.2, 1.7, 2.9)]
+    [DataRow(5.3, 1, 6.3)]
+    public void AdditionTest(double left, double right, double expected)
     {
+        var result = CalculatorLogic.CalculatorLogic.Addition(left, right);
+        Assert.AreEqual(expected, result);
     }
+    
 }
